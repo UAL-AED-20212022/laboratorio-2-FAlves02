dd64edf53649ca26dec1fc181ef8caf5002f7191
Francisco Alves <2002franciscoalves@gmail.com>
2022-04-26 10:15:28 +0100

    .

---
+++ controllers/controller.py
@@ -0,0 +1,43 @@
+from models import LinkedList
+
+lista = LinkedList
+
+
+def inserir_inicio(scan):
+    lista.insert_at_start(scan[1])
+
+
+def inserir_fim(scan):
+    lista.insert_at_end(scan[1])
+
+
+def inserir_depois(scan):
+    lista.insert_after_item(scan[2], scan[1])
+
+
+def inserir_antes(scan):
+    lista.insert_before_item(scan[2], scan[1])
+
+
+def inserir_indice(scan):
+    lista.insert_at_index(scan[2], scan[1])
+
+
+def verificar_numero():
+    lista.get_count()
+
+
+def verificar_pais(scan):
+    lista.search_item(scan[1])
+
+
+def eliminar_inicio():
+    lista.delete_at_start()
+
+
+def eliminar_fim():
+    lista.delete_at_end()
+
+
+def eliminar_pais(scan):
+    lista.delete_element_by_value(scan[1])
---
+++ views/view.py
@@ -0,0 +1,33 @@
+from controllers.controller import inserir_inicio, inserir_fim, inserir_depois, inserir_antes, inserir_indice, \
+    verificar_pais, eliminar_inicio, eliminar_fim, eliminar_pais, verificar_numero
+
+def run():
+    try:
+        while True:
+            scan = input().split()
+            match scan[0].upper():
+                case "RPI":
+                    inserir_inicio(scan)
+                case "RPF":
+                    inserir_fim(scan)
+                case "RPDE":
+                    inserir_depois(scan)
+                case "RPAE":
+                    inserir_antes(scan)
+                case "RPII":
+                    inserir_indice(scan)
+                case "VNE":
+                    verificar_numero()
+                case "VP":
+                    verificar_pais(scan)
+                case "EPE":
+                    eliminar_inicio()
+                case "EUE":
+                    eliminar_fim()
+                case "EP":
+                    eliminar_pais(scan)
+                    pass
+                case "":
+                    break
+    except:
+        pass
